792d4e934a6f06a5e65b2d27ef0dc825f61dff22
bogachevskes <ace7upp@gmail.com>
2023-02-25 19:26:51 +0300

    замыкающая запятая

--- web/index.php
+++ web/index.php
@@ -6,3 +6,4 @@ include_once __DIR__ . '/../src/01_named_arguments.php';
 include_once __DIR__ . '/../src/02_union_types.php';
 include_once __DIR__ . '/../src/03_constructor_property_promotion.php';
 include_once __DIR__ . '/../src/04_nullsafe_operator.php';
+include_once __DIR__ . '/../src/05_trailing_comma_in_parameters.php';
